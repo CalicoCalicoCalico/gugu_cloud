2db714c905e0ce53348c37ea7f1d144450d59428
PUNTHLHU <145959995+PUNTHLHU@users.noreply.github.com>
2026-06-29 10:33:42 +0900

    loop.js (#23)
    - 뚠띠 걷는 모습 추가
    - loop.js: 엔딩 영상 때 브금 멈추게 하는거 추가 및 영상 길이대로 초수 수정
    - main.js: 버튼 누르면 바로 브금 재생 안되게 삭제
    - 비디오 에셋 폴더에 인트로, 엔딩 영상 추가

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ function onTitleStart() {
     clearHumanLayer(); // (이전 게임의 발 DOM 정리)
     seedCigarettes(DATA.CONFIG.SPAWN.INITIAL_COUNT); // 맵에 시작 시 생성되어있는 담배 갯수
     switchScene("introVideo");
-    startBgm(); // 사용자 클릭 시점 → 자동재생 정책 통과
 }
 
 /**
